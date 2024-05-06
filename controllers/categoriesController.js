d175f906eebb9bb2d70833623545d5739bd91e3b
fprophet <eduard.ciuban96@gmail.com>
2024-05-06 11:26:32 -0700

    created category form and controller  create function

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -1,6 +1,30 @@
+const Category = require("../models/categories");
+
 const Controller = {
   renderIndex: (req, res) => {
     res.render("../views/categories/index");
+  },
+
+  createCategory: async (req, res, next) => {
+    if (!req.body.name) {
+      res.send({ message: "Please fill all fields!", status: "failed" });
+      return next();
+    }
+
+    const found = await Category.findOne({ name: req.body.name });
+    if (found) {
+      res.send({
+        message: "Category with this name already exists",
+        status: "failed",
+      });
+      return next();
+    }
+
+    await Category.create({ name: req.body.name });
+    res.send({
+      message: "Category created!",
+      status: "success",
+    });
   },
 };
 
--- routes/categories.js
+++ routes/categories.js
@@ -5,5 +5,6 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", controller.renderIndex);
+router.post("/create", controller.createCategory);
 
 module.exports = router;
